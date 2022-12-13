f880ca6910e98a2de33015710821b63b0a052c4c
yegod1101 <39053263+cooldae@users.noreply.github.com>
2022-12-13 11:27:49 +0900

    First file

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      
+    </div>
+    <div>
+      <Main></Main>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Main from '@/views/Main.vue'
+    export default {
+        name: 'App',
+        data() {
+            return {
+
+            }
+        },
+        components: {
+          Main
+        }
+    }
+    
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#nav {
+  margin-bottom: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding-right: 10px;
+}
+
+#nav a:not(:first-child) {
+  padding-left: 20px; 
+}
+
+.topnav {
+  background-color: #EAEAEA;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  padding: 10px 15px;
+  text-decoration: none;
+  font-size: 9px;
+}
+</style>
---
+++ src/views/Testview.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="Testview">
+    <button @click="createroom">Create</button>
+    <div class="container">
+      <Test/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Test from '@/components/test.vue'
+
+export default {
+  name: 'testview',
+  components: {
+    Test
+  },
+  data() {
+    return {
+      roominfo: [
+        {
+          name: '',
+          content: '',
+          private: false,
+        }
+
+      ]
+    }
+  },
+
+  methods: {
+    createroom() {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+#Testview {
+  display: auto;
+}
+.container {
+  border: 1px solid black;
+  width: 700px;
+  height: 500px;
+}
+
+</style>
